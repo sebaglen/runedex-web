1eb132571b7ebac88086e1949cb304285b1ed344
Sebastian Aglen Danielsen <sd@udp.no>
2020-07-06 19:47:20 +0200

    changed firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,20 +1,15 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
+// The configuration below is not sensitive data. You can serenely add your config here
+const config = {
+  apiKey: "AIzaSyClwIFPaBzPYxhGbsGqLxjIRAUngdQeQiw",
+  authDomain: "runedex-77a4c.firebaseapp.com",
+  databaseURL: "https://runedex-77a4c.firebaseio.com",
+  projectId: "runedex-77a4c",
+  storageBucket: "runedex-77a4c.appspot.com",
+  messagingSenderId: "100766885712",
+  appId: "1:100766885712:web:879df63a0d32e96b3e2e55"
+}
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyClwIFPaBzPYxhGbsGqLxjIRAUngdQeQiw",
-    authDomain: "runedex-77a4c.firebaseapp.com",
-    databaseURL: "https://runedex-77a4c.firebaseio.com",
-    projectId: "runedex-77a4c",
-    storageBucket: "runedex-77a4c.appspot.com",
-    messagingSenderId: "100766885712",
-    appId: "1:100766885712:web:879df63a0d32e96b3e2e55"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+firebase.initializeApp(config)
