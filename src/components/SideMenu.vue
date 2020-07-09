5cffe452edf06a3d6846155b47510c1e0d6c64f6
Sebastian Aglen Danielsen <sd@udp.no>
2020-07-09 22:30:13 +0200

    added mobile ui switch and some stuff

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sideMenu">
+    THIS IS A SIDE MENU
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@/theme/variables.scss';
+
+.sideMenu {
+  z-index: 20;
+  height: calc(100% - $navbar-height);
+  width: 200px;
+  background-color: $gray;
+  margin-top: $navbar-height;
+  box-sizing: border-box;
+
+}
+</style>
