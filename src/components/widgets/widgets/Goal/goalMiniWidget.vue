3de5d7f557a8a79a95d81e0f4165eeb15db7e30c
mortenhillbom <morten.hillbom@cognite.com>
2020-07-12 22:36:45 +0200

    Add manifest files for goal and bankvalue widgets

---
+++ src/components/widgets/widgets/Goal/goalMiniWidget.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Goal</h1>
+    <p>{{ uniqueId }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // use this to fetch proper config and data
+    uniqueId: Number
+  }
+}
+</script>
+
+<style scoped></style>
