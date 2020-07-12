a36adbb8c8f22185df99f52903b73610bf7a6569
Sebastian Aglen Danielsen <sd@udp.no>
2020-07-12 19:50:04 +0200

    added initial widget structure

--- src/components/widgets/Widgets.vue
+++ src/components/widgets/Widgets.vue
@@ -1,12 +1,20 @@
 <template>
-  <div :class="[isMobile ? 'mobile' : '', 'widgets']">
-      I am a widget panel
-  </div>
+    <div :class="[isMobile ? 'mobile' : '', 'widgets']">
+        <div class="last-seen">
+            <div>last seen: 3 hors ago</div>
+        </div>
+        <div class="widget-container flex-row">
+            <widget-shell v-for="widget in data" :key="widget.uniqueId" :type="widget.type"></widget-shell>
+            <widget-shell type="addnew"></widget-shell>
+        </div>
+    </div>
 </template>
 
 <script>
+import WidgetShell from '@/components/widgets/widgets/mini/widgetShell'
 
 export default {
+  components: { WidgetShell },
   computed: {
     isMobile() {
       if (this.window.width <= 750) {
@@ -19,7 +27,71 @@ export default {
     return {
       window: {
         width: 0,
-      }
+      },
+      data: [
+            {
+                uniqueId: 1,
+                type: "bankvalue",
+                title: "Bank value",
+                config: {},
+            },
+            {
+                uniqueId: 2,
+                type: "totallevel",
+                title: "Total level",
+                config: {},
+            },
+            {
+                uniqueId: 3,
+                type: "xpgained",
+                title: "Experience gained",
+                config: {
+                    time: "month"
+                },
+            },
+            {
+                uniqueId: 4,
+                type: "goal",
+                title: "Complete goal",
+                config: {
+                    levels: [
+                        { attack: 95 },
+                        { strength: 95 },
+                        { defence: 70 },
+                    ],
+                    quests: [
+                        { questId: "dragon slayer 2" }, // get quest id later sometime
+                    ],
+                    items: [
+                        { 
+                            id: 995, // coins
+                            amount: 60000
+                        },
+                    ]
+                },
+            },
+            {
+                uniqueId: 5,
+                type: "goal",
+                title: "Complete goal",
+                config: {
+                    levels: [
+                        { attack: 60 },
+                        { strength: 60 },
+                        { defence: 60 },
+                    ],
+                    quests: [
+                        { questId: "dragon slayer" }, // get quest id later sometime
+                    ],
+                    items: [
+                        { 
+                            id: 995, // coins
+                            amount: 2000
+                        },
+                    ]
+                },
+            },
+      ]
     }
   },
   methods: {
@@ -42,6 +114,15 @@ export default {
 .widgets {
   width: 320px;
   background-color: $neutral;
+  .widget-container{
+    overflow: hidden;
+    flex-wrap: wrap;
+  }
+  .last-seen {
+    margin: 5px 0 0 13px;
+    color: $lighter;
+    font-size: 14px;
+  }
 }
 
 .mobile {
---
+++ src/components/widgets/widgets/mini/widgetShell.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="widget">
+      {{type}}
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/theme/variables.scss';
+.widget {
+  min-width: 140px;
+  min-height: 140px;
+  border-radius: 10px;
+  background-color: $light;
+  text-align: center;
+  font-size: 0.8em;
+  margin: 13px 0 0 13px;
+  cursor: pointer;
+}
+</style>
