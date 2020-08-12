968a7b8054ce7fb06f7c10dc9aa86ff77fe92c34
Sebastian Aglen Danielsen <sd@udp.no>
2020-08-12 18:42:07 +0200

    added total level mini widget

--- src/components/widgets/widgets/TotalLevel/totalLevelMiniWidget.vue
+++ src/components/widgets/widgets/TotalLevel/totalLevelMiniWidget.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>
-    <h1>Total Level</h1>
-    <p>{{ uniqueId }}</p>
+  <div class="mini-widget-container">
+    <h3>Combat level</h3>
+    <div class="flex-row">
+      <p>Week</p>
+      <h3 class="gain">+ 4</h3>
+    </div>
+    <h1>{{totalXp}}</h1>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  props: {
-    // use this to fetch proper config and data
-    uniqueId: String
-  }
+  computed: {
+    ...mapGetters('accounts', ['getAccountById']),
+    totalXp() {
+      return this.getAccountById(this.$route.params.accountId).character.combatLevel;
+    }
+  },
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import '@/theme/variables.scss';
+.mini-widget-container {
+  text-align: left;
+  color: $black;
+  margin: 10px;
+}
+
+h1 {
+  color: $primary;
+  text-align: center;
+  margin-top: -4px;
+}
+
+.gain {
+  color: $primary;
+  margin: -5px 0 0 auto;
+  text-align: right;
+}
+</style>
